be58ffb06fd486118071e90ee3dffa709483922d
Vlatko Juric <juricv78gmail.com>
2023-10-06 12:32:16 +0200

    solved the first two tasks exept the hide answer on the button

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkColorChange = document.querySelector(
+  '[data-js="bookmark-change-color"]'
+);
+
+bookmarkColorChange.addEventListener("click", () => {
+  bookmarkColorChange.classList.toggle("bookmark--active");
+});
+
+const showAnswerButton = document.querySelector(
+  '[data-js="show-answer-button"]'
+);
+
+const answer = document.querySelector('[data-js="answer"]');
+
+showAnswerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer");
+});
