0ebc01c8c7f77a7373897a3740115421232b6a6c
Vlatko Juric <juricv78gmail.com>
2023-10-08 15:00:59 +0200

    added comments to the JS file so I know what I did with each line on code,maybe it can help someone who is stuck on this part with the explenation

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // phew… not a lot going on here. Please add some code!
+
+//added the data js from  the html element to grab it from the html file and put it into a variable
 const bookmarkColorChange = document.querySelector(
   '[data-js="bookmark-change-color"]'
 );
-
+// use that variable to add evenet listener to listen for click and do a function,in this case the bookmarks should toggle on click
 bookmarkColorChange.addEventListener("click", () => {
   bookmarkColorChange.classList.toggle("bookmark--active");
 });
-
+//we put the button into a variable
 const showAnswerButton = document.querySelector(
   '[data-js="show-answer-button"]'
 );
-
+//we put the answer into a variable also so we can use it in a function down below
 const answer = document.querySelector('[data-js="answer"]');
+//we added a event listener on a button in html file which we saved into a variable so when we click it does something
+//then we use the answe variable in which the hidden answer is stored
+//we use the conditionals,and if the saved variable "contains" the answer,the answer will show and the button will change it´s "textContent" to Hide answer,so the answer hides again
 
 showAnswerButton.addEventListener("click", () => {
   answer.classList.toggle("card__answer--active");
